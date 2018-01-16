0628e8cfcb0e808e0a08e07d85074c02470ebd40
Gregory Mierzwinski <31373005+eterno-hc@users.noreply.github.com>
2018-01-16 14:23:30 -0500

    Add example graphs.

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Pupil-Lib Python
 
-# Usage
+## Usage
 
 Either use it in a script with calls that resemble the `main()` function in pupil_lib.py. `yaml_path` must be defined
 in the `get_build_config(yaml_path=<PATH/TO/YAML>)` call.
@@ -14,15 +14,33 @@ You can use it through the command prompt as well with something like:
 Or with only this to get the arguments from a YAML configuration file (defined in the docs/ folder):
 `python pupil_lib.py --run-config C:\Users\Gregory\PycharmProjects\pupil_lib_parallel_exp\resources\test_yaml1.yml`
 
-# Data Usage
+## Data Usage
 
 data_container.py shows the general structure of the data once it's finished processing. Generally speaking, accessing data
 will be similar in all cases to what is done in `main()` in pupil_lib.py.
 
-# Marker creation
+## Marker creation
 
 Using the Pupil Labs LSL plugin, you can create and send markers from a stimulus script in the same way that is done here:
  https://github.com/sccn/labstreaminglayer/blob/master/LSL/liblsl-Python/examples/SendStringMarkers.py
 
 The stream can/will be saved by the Lab Recorder software and that data can then be used for processing in this library.
 (For the stimulus scripts, they can be in any language that LSL offers so that markers can be created and sent).
+
+## Examples of data that can be retrieved
+
+These images below are from processing a dataset where a subject was looking at these stimuli:
+
+![alt text](https://user-images.githubusercontent.com/10966989/35007458-ce26d07a-fac7-11e7-9817-1e2c3f2bfc9e.png)
+
+Gaze data (gaze_x, and gaze_y fields):
+
+![alt text](https://user-images.githubusercontent.com/10966989/35007537-f99df72e-fac7-11e7-9daa-d035ca92bd42.png)
+
+All trials across all triggers:
+
+![alt text](https://user-images.githubusercontent.com/10966989/35007535-f97cf86c-fac7-11e7-9eab-8949a9961a7e.png)
+
+Mean of all trials for each trigger overlaid:
+
+![alt text](https://user-images.githubusercontent.com/10966989/35007562-121dbcf8-fac8-11e7-9acd-c14bd579fdef.png)
--- pupil_lib.py
+++ pupil_lib.py
@@ -467,13 +467,23 @@ def main():
     for i in dat_mat:
         print(i)
         plt.subplot(2, 1, 1)
-        plt.plot(i)
+        plt.plot(np.linspace(0, 4000, num=len(i)), i)
+        plt.xlabel('Time (milli-seconds)')
+        plt.ylabel('X Eye Movement (gaze x)')
+    plt.axhline(0, color='r')
+    plt.axvline(1000, color='r')
+    plt.axvline(3000, color='r')
 
     dat_mat = datastore.datasets['dataset1'].data_streams['gaze_y'].triggers['S11'].get_matrix()
     plt.subplot(2, 1, 2)
     for i in dat_mat:
         print(i)
-        plt.plot(i)
+        plt.plot(np.linspace(0, 4000, num=len(i)), i)
+        plt.xlabel('Time (milli-seconds)')
+        plt.ylabel('Y Eye Movement (gaze y)')
+    plt.axhline(0, color='r')
+    plt.axvline(1000, color='r')
+    plt.axvline(3000, color='r')
 
     datastore.data_type = 'pc'
 
@@ -482,17 +492,26 @@ def main():
         plt.subplot(2, 2, i+1)
         dat_mat = datastore.datasets['dataset2'].data_streams['eye0'].triggers[trigs[i]].get_matrix()
         for trial in dat_mat:
-            plt.plot(trial)
-        plt.axhline(0)
-        plt.axvline(256)
-        plt.axvline(768)
+            plt.plot(np.linspace(0, 4000, num=len(trial)), trial)
+        plt.title('Trigger: ' + trigs[i])
+        if i >= 2:
+            plt.xlabel('Time (milli-seconds)')
+        plt.ylabel('Percent Change Diameter')
+        plt.axhline(0, color='r')
+        plt.axvline(1000, color='r')
+        plt.axvline(3000, color='r')
 
     plt.figure()
     for trig in trigs:
-        plt.plot(np.mean(datastore.datasets['dataset2'].data_streams['eye0'].triggers[trig].get_matrix(), 0), col[trig])
-    plt.axhline(0)
-    plt.axvline(256)
-    plt.axvline(768)
+        line_y = np.mean(datastore.datasets['dataset2'].data_streams['eye0'].triggers[trig].get_matrix(), 0)
+        plt.plot(np.linspace(0, 4000, num=len(line_y)), line_y,
+                 col[trig], label=trig)
+    plt.legend()
+    plt.xlabel('Time (milli-seconds)')
+    plt.ylabel('Percent Change Diameter')
+    plt.axhline(0, color='r')
+    plt.axvline(1000, color='r')
+    plt.axvline(3000, color='r')
 
     plt.show(block=True)
 
