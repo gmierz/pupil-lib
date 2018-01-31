0cfa13c4d08da7159a0839b01a6995008e32be0e
Greg Mierzwinski <gmierz1@live.ca>
2018-01-31 11:14:28 -0500

    Removing cached files.

--- core/starter.py
+++
@@ -1,6 +0,0 @@
-import os
-
-print(os.getcwd())
-os.system("py core/pupil_lib.py --datasets C:\\Users\\Gregory\\Documents\\Honors_Thesis\\2017_03_07\\002 "
-          "C:\\Users\\Gregory\\Documents\\Honors_Thesis\\2017_03_07\\002 --triggers S11 S12 S13 "
-          "--trial-range -1 2 --baseline 1 --store artifacts/2017_06_24/ --logger stdout --max-workers 17")
--- core/workers/processors/processing_functions/testing_functions.py
+++
@@ -1,3 +0,0 @@
-
-def a_test_to_do(string_to_print):
-    print(string_to_print)
