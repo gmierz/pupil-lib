d53ee411e55e16cc82ffc00a2021e2ce513d69a6
Greg Mierzwinski <gmierz1@live.ca>
2018-06-30 19:55:03 -0400

    Add example for getting raw markers indices in raw timeseries.

---
+++ pupillib/example_only_get_marker_positions.py
@@ -0,0 +1,53 @@
+import numpy as np
+import time
+
+from pupillib.pupil_lib import script_run, save_csv_line
+
+# Requires a 3D dataset with the form: [trials, values]
+# Open a figure befor calling this.
+# If standard_error is false then standard deviations will be plotted.
+def get_std_dev(matrix, standard_error=True):
+    std_tmp = []
+    for point in range(matrix.shape[1]):
+        std_tmp.append(np.std(np.squeeze(matrix[:, point])))
+        if standard_error:
+            std_tmp[point] = std_tmp[point] / np.sqrt(matrix.shape[1])
+    return std_tmp
+
+def main():
+    # Load the datasets and run
+    plibrunner = script_run(
+        yaml_path='resources/test_yaml2.yml'
+    )
+
+    # After this the plibrunner will hold information about the datasets,
+    # and it can be stored for viewing, and extra processing later.
+    datastore = plibrunner.data_store
+    len0 = len(datastore.datasets['dataset1'].data_streams['eye0'].data)
+    len1 = len(datastore.datasets['dataset1'].data_streams['eye1'].data)
+    print('len0: ' + str(len0))
+    print('len1: ' + str(len1))
+    from matplotlib import pyplot as plt
+
+    datastore.time_or_data = 'data'
+    trigs = ['S11', 'S12', 'S13', 'S14']
+    trigger_names = {'S11':'White', 'S12':'Red', 'S13':'Green', 'S14':'colored'}
+    col = {'S14': 'blue', 'S11': 'darkorange', 'S13': 'g', 'S12': 'r'}
+    datastore.data_type = 'pc'
+    alpha = 0.3
+
+    for dataname in datastore.datasets['dataset1'].data_streams:
+        dstream = datastore.datasets['dataset1'].data_streams[dataname] # Get raw data
+        plt.figure()
+        plt.plot(dstream.data, color='black')
+        for trig in trigs:
+            trig_indices = dstream.trigger_indices[trig] # Get marker indices
+            for ind in trig_indices:
+                plt.axvline(x=ind, color=col[trig])
+
+    plt.show(block=True)
+
+    print('Main Terminating...')
+
+if __name__ == "__main__":
+    main()
